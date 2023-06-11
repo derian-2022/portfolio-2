2d2c0960e9f1977ea5e13e0fb606aeb3f7d1cc5a
derian-2022 <martinarias.edu@gmail.com>
2023-06-11 18:04:28 -0500

    Portfolio 2

---
+++ pages/post.js
@@ -0,0 +1,39 @@
+import Layout from "../components/Layout";
+import { useRouter } from "next/router";
+import { posts } from "../profile";
+
+const Post = () => {
+  const router = useRouter();
+
+  const currentPost = posts.filter(
+    (post) => post.title === router.query.title
+  )[0];
+
+  if (!currentPost) {
+    return (
+      <Layout title={router.query.title} footer={false}>
+        <div className="text-center">
+          <p>El post no fue encontrado.</p>
+        </div>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout title={router.query.title} footer={false}>
+      <div className="text-center">
+        {currentPost.imageURL && (
+          <img
+            src={currentPost.imageURL}
+            alt=""
+            style={{ width: "50%" }}
+            className="img-fluid"
+          />
+        )}
+        <p className="p-4">{currentPost.content}</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Post;
